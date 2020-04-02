c7d71c577cdf49b14679ca4ac19c504b03265615
Santhosh <vijapur.santhu1234@gmail.com>
2020-04-02 21:11:32 +0530

    Updates

---
+++ precache-manifest.98774101036a28315d7a96789770660a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097893b56d9af0098d880b28334ae106",
+    "url": "/samweb/index.html"
+  },
+  {
+    "revision": "555721809a5952b7d64e",
+    "url": "/samweb/static/js/2.71e98813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samweb/static/js/2.71e98813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4fdd27bee307e3c272",
+    "url": "/samweb/static/js/main.a6305a6e.chunk.js"
+  },
+  {
+    "revision": "8824933b16d0865fecb9",
+    "url": "/samweb/static/js/runtime-main.609a993e.js"
+  }
+]);
